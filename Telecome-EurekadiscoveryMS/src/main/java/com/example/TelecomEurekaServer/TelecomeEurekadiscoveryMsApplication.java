8f2923d146a764da7df83d00ea6fa9c77d254ea1
shubhanshu611 <106374980+shubhanshu611@users.noreply.github.com>
2023-04-04 14:16:42 +0530

    Add files via upload

---
+++ Telecome-EurekadiscoveryMS/src/main/java/com/example/TelecomEurekaServer/TelecomeEurekadiscoveryMsApplication.java
@@ -0,0 +1,15 @@
+package com.example.TelecomEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class TelecomeEurekadiscoveryMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomeEurekadiscoveryMsApplication.class, args);
+	}
+
+}
